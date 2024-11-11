8681691a7d0be7f610d97ab4335b62821c33638a
Alessandro <alessandrofrancisco729@gmail.com>
2024-11-11 14:41:12 -0300

    Foi adicionado a parte de seleção

--- src/route-user.js
+++ src/route-user.js
@@ -8,16 +8,30 @@ const prod_router = Router();
 
 prod_router.post("/produto", (req, res) =>{
     const {nome, validade, preco} = req.body;
-    conn.query(`insert into produtos(nome , validade, preco)
-        valeus ('${nome}', '${preco}','${validade }')`,(err,result) =>{
+    conn.query(`insert into produtos(nome , validade, preco, categoria)
+        valeus ('${nome}', '${validade}','${preco }','${categoria}')`,(err,result) =>{
             if (err){
                 return res.json({
                     Erro: "Erro na inserção de dados," +err.message
                 });
             };
-            res.json({
+            res.json({              
                 Sucesso: `Produto ${nome} cadastro com sucesso!`
             });
         });
 });
+prod_router.post("/lista", (req, res) =>{
+    conn.query('select * from produtos',(err,result) =>{
+        if (err){
+            res.json({
+                Erro: "Erro ao consultar dados !!!!" + err.message
+            })
+        }
+        res.json(result);
 
+        result.map((item ) => {
+            console.log(item.nome);
+        });
+    });
+});
+export{prod_router};
