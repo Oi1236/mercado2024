bb3c931b6939f96864bc590c94758bbdd642bbd5
Alessandro <alessandrofrancisco729@gmail.com>
2024-11-11 12:56:30 -0300

    router dos produtos

--- src/route-user.js
+++ src/route-user.js
@@ -4,8 +4,20 @@ import {conn} from "../src/mercado-bd"
 
 const prod_router = Router();
 
+
+
 prod_router.post("/produto", (req, res) =>{
     const {nome, validade, preco} = req.body;
+    conn.query(`insert into produtos(nome , validade, preco)
+        valeus ('${nome}', '${preco}','${validade }')`,(err,result) =>{
+            if (err){
+                return res.json({
+                    Erro: "Erro na inserção de dados," +err.message
+                });
+            };
+            res.json({
+                Sucesso: `Produto ${nome} cadastro com sucesso!`
+            });
+        });
+});
 
-    conn.query
-})
