37b6f11c5903e5fa97e73bf227c9b39c5cf8e078
Alessandro <alessandrofrancisco729@gmail.com>
2024-11-13 12:02:18 -0300

    Correção de todo o código

--- src/route-user.js
+++ src/route-user.js
@@ -1,65 +1,71 @@
 import { Router } from "express";
-import {conn} from "../src/mercado-bd"
+import { conn } from "../src/mercado-bd.js"
 
 
 const prod_router = Router();
 
 
 
-prod_router.post("/produto", (req, res) =>{
-    const {nome, validade, preco, categoria} = req.body;
+prod_router.post("/produto", (req, res) => {
+    const { nome, validade, preco, categoria } = req.body;
     conn.query(`insert into produtos(nome , validade, preco, categoria)
-        values ('${nome}', '${validade}','${preco }','${categoria}')`,(err,result) =>{
-            if (err){
-                return res.json({
-                    Erro: "Erro na inserção de dados," +err.message
-                });
-            };
-            res.json({              
-                Sucesso: `Produto ${nome} cadastro com sucesso!`
+        values ('${nome}', '${validade}','${preco}','${categoria}')`, (err, result) => {
+        if (err) {
+            return res.json({
+                Erro: "Erro na inserção de dados," + err.message
             });
-        });
-});
-prod_router.post("/lista", (req, res) =>{
-    conn.query('select * from produtos',(err,result) =>{
-        if (err){
-            res.json({
-                Erro: "Erro ao consultar dados !!!!" + err.message
-            })
-        }
-        res.json(result);
-
-        result.map((item ) => {
-            console.log(item.nome);
+        };
+        res.json({
+            Sucesso: `Produto ${nome} cadastro com sucesso!`
         });
     });
 });
-prod_router.post("/atualizar", (req,res) => {
-    const {nome,validade,preco, categoria} = req.body;
-    conn.query(`update into produtos(nome, validade, preco, categoria)
-        values ('${nome},${validade},${preco},${categoria}')`, (err,result) =>{
-            if (err){
-                return res.json({
-                    Erro: "Erro ao atualizar dados !!!!" + err.massage
-                });
-            }
-            res.json({
-                Sucesso: `Produto ${nome}, nome atualizado`
-            });
-        });
 
-});
-prod_router.post('/deletar', (req,res) => {
-    const{nome, validade, preco, categoria} = req.body;
-    conn.query(`${nome},${validade}, ${preco},${categoria}`, (err,result) => {
-        if (err){
-            return res.json({
-                Erro: "Erro ao deletar dados !!!!" + err.massage
-            });
-        }
-        res.json({
-            Sucesso: `Produto ${nome}, apagado`
-        })
-    })
-})
-export{prod_router};
+// prod_router.post("/lista", (req, res) =>{
+//     conn.query('select * from produtos',(err,result) =>{
+//         if (err){
+//             res.json({
+//                 Erro: "Erro ao consultar dados !!!!" + err.message
+//             })
+//         }
+//         res.json(result);
+
+//         result.map((item ) => {
+//             console.log(item.nome);
+//         });
+//     });
+// });
+
+// prod_router.post("/atualizar", (req,res) => {
+//     const {nome,validade,preco, categoria} = req.body;
+//     conn.query(`update into produtos(nome, validade, preco, categoria)
+//         values ('${nome},${validade},${preco},${categoria}')`, (err,result) =>{
+//             if (err){
+//                 return res.json({
+//                     Erro: "Erro ao atualizar dados !!!!" + err.massage
+//                 });
+//             }
+//             res.json({
+//                 Sucesso: `Produto ${nome}, nome atualizado`
+//             });
+//         });
+
+// });
+
+// prod_router.post("/deletar" , (res,req) =>{
+//     const {nome,validade,preco,categoria} = req.body;
+//     conn.query(`delete into produtos (nome,validade,preco,categoria
+//         valeus ('${nome},${validade},${preco},${categoria}') )` , (err,result) => {
+//             if (err){
+//                 return res.json({
+//                     Erro: "Erro ao deletar dados !!!!" + err.message
+//                 });
+//             }
+//             res.json({
+//                 Sucesso:`Produto deletado com sucesso!!!`
+//             });
+//         });
+// });
+
+
+export { prod_router };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-import express from "express"
+import express, { Router } from "express"
+import { prod_router } from "./route-user.js";
 
 
 const app = express();
 
 app.use(express.json());
-app.use()
+app.use(prod_router)
+
 
 app.listen(3333, () => console.log('Servidor rodando...'));
